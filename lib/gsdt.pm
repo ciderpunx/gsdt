1ddddab38e4dfa0dfd329c04bc99fad587a5b811
Charlie Harvey <charlie@charlieharvey.org.uk>
2014-02-22 22:19:00 +0000

    Added the authentication handling code aand skeleton code

--- lib/gsdt.pm
+++ lib/gsdt.pm
@@ -1,15 +1,48 @@
 package gsdt;
 use Dancer ':syntax'; 
 use Dancer::Plugin::Cache::CHI;
+use Dancer::Plugin::Auth::Extensible;
 
 our $VERSION = '0.1';
 
 check_page_cache;
 
+get '/dashboard' => require_login sub {
+	template "index", {
+		info => "You are on the dashboard",	
+		title => "Get Stuff Done. Today.",
+		description => "A task management system.",
+	};
+};
+
 get '/' => sub {
-	template "index";
+	template "index", {
+		title => "Get Stuff Done. Today.",
+		description => "A task management system.",
+	};
 };
 
 
+any '/login' => sub {
+    my ( $success, $realm ) =
+      authenticate_user( params->{username}, params->{password} );
+    if ($success) {
+        session logged_in_user       => params->{username};
+        session logged_in_user_realm => $realm;
+				redirect uri_for '/dashboard';
+    }
+    else {
+			template "index", {
+				errors => ["You can't log in. Check your spelling?"], 
+			};
+    }
+};
+
+any '/logout' => sub {
+	session->destroy;
+	template "index", {
+		info => "Logged in.", 
+	}
+};
 
 true;
